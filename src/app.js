7c994994b78aa88bf7a8c671b93cd9a6467346aa
Luis Sarno <lulujr96@gmail.com>
2023-01-20 14:42:27 -0500

    created a branch that has no navbar

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const hamburgerButton = document.getElementById('hamburger')
+console.log(hamburgerButton);
 const navList = document.getElementById('nav-list')
+console.log(navList);
+
 
 function toggleButton() {
-    navList.classList.toggle('show')
+    console.log("Toggle button function called.");
+    navList.classList.toggle('show');
 }
 
-hamburgerButton.addEventListener('click', toggleButton)
+hamburgerButton.addEventListener('click', () => {console.log('Event listener attached'); toggleButton(); });
